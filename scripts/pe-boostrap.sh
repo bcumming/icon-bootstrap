bc2a052ca83c20e9849bf18612f47c9aeafe487e
bcumming <bcumming@cscs.ch>
2023-10-06 09:45:45 +0200

    first draft of repo bootstrapping

---
+++ scripts/pe-boostrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+header() {
+    echo ====== $1
+}
+
+
+pyenv_path="$(pwd)/.pyenv-bootstrap"
+rm -rf $pyenv_path
+header "creating python environment $pyenv_path"
+python3 -m venv $pyenv_path
+source $pyenv_path/bin/activate
+
+export stackinator_path="$PWD/software/stackinator"
+export spack_path="$PWD/software/spack"
+
+# bootstrap python packages required by stackinator
+header "pip installing the python packages in $(pwd)/repos/bootstrap-requirements.txt"
+pip --quiet install --no-index --find-links=./repos/bootstrap-python -r ./repos/bootstrap-requirements.txt
+pip list
+
+export PATH="$stackinator_path/bin:$PATH"
+export PATH="$spack_path/bin:$PATH"
+
+
+# setting up spack bootstrap
+spack --version
+spack_bootstrap="$(pwd)/repos/bootstrap-spack"
+spack bootstrap add --trust local-sources $spack_bootstrap/metadata/sources
+spack bootstrap add --trust local-binaries $spack_bootstrap/metadata/binaries
+
+spack bootstrap status
---
+++ scripts/repo-bootstrap.sh
@@ -0,0 +1,42 @@
+#/bin/bash
+
+header() {
+    echo ====== $1
+}
+
+rm -rf repo
+repo_path=$(pwd)/repo
+sw_repo=${repo_path}/software
+pip_repo=${repo_path}/pip
+spack_repo=${repo_path}/spack
+
+etc_path=$(pwd)/pip
+
+penv_path=$(pwd)/.pyenv-bootstrap
+rm -rf ${penv_path}
+
+header "creating python virtual environment in .pyenv-download"
+python3 -m venv ${penv_path}
+source ${penv_path}/bin/activate
+
+pip --quiet install pip --upgrade
+
+mkdir -p ${sw_repo}
+header "cloning stackinator"
+git clone --quiet --depth=1 --branch "v3.0" git@github.com:eth-cscs/stackinator.git ${sw_repo}/stackinator
+header "cloning spack"
+git clone --quiet --depth=1 --branch "v0.20.2" git@github.com:spack/spack.git ${sw_repo}/spack
+header "cloning uenv"
+git clone --quiet --depth=1 git@github.com:eth-cscs/uenv.git ${sw_repo}/uenv
+
+header "downloading the bootstrap python dependencies"
+pip --quiet download --destination-directory ${pip_repo}/bootstrap -r etc/bootstrap-requirements.txt
+
+header "downloaded python packages"
+ls ${pip_repo}/bootstrap
+
+wget https://bootstrap.pypa.io/get-pip.py
+mv get-pip.py ${pip_repo}
+
+header "setting up the spack bootstrap mirror"
+${sw_repo}/spack/bin/spack bootstrap mirror --binary-packages ${spack_repo}/bootstrap
